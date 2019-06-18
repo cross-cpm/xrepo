a9d791e78fcf955ed1f140b3b7449de3582db9e8
charles <qfl2000@gmail.com>
2019-06-18 23:40:45 +0800

    coding

--- repo/externals.go
+++ repo/externals.go
@@ -1,6 +1,19 @@
 package repo
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type repoDefine struct {
+	Branch  string              `json:"branch"`
+	Ref     string              `json:"ref"`
+	Targets map[string][]string `json:"targets"`
+}
+
 type externals struct {
+	defines map[string]repoDefine
 }
 
 func NewExternals() *externals {
@@ -8,6 +21,20 @@ func NewExternals() *externals {
 }
 
 func (e *externals) Load(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Println("open file failed!", err)
+		return err
+	}
+	defer f.Close()
+
+	err = json.NewDecoder(f).Decode(&e.defines)
+	if err != nil {
+		log.Println("decode externals json failed!", err)
+		return err
+	}
+
+	log.Println("debug externals:", e.defines)
 	return nil
 }
 
